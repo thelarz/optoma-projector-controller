06fb958fc5eaac72f3218754787fbf5b04041ff6
Larry McNutt <larry@mcnutt-consulting.com>
2017-10-25 14:44:26 -0500

    Credits

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+// thanks:
+// https://bretkikehara.wordpress.com/2013/05/02/nodejs-creating-your-first-global-module/
+
 var controller = require("projector-controller");
 
 var command = process.argv[2];
